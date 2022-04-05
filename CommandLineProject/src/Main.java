f838d10d59e0b76b1bfc3e66a17567bfa5a58412
Suzanne Zomer <suzannezomer@gmail.com>
2022-04-05 10:39:31 +0200

    Update Web and CLI with new products + descr

--- CommandLineProject/src/Main.java
+++ CommandLineProject/src/Main.java
@@ -12,8 +12,42 @@ public class Main {
     private String option;
 
     private String[][] products = {
-            {"apple", "pear"},
-            {"This is an apple", "This is a pear"}
+            {"apple", "Apples are not native to North America. They originated in Kazakhstan, in central Asia east " +
+                    "of the Caspian Sea. The capital of Kazakhstan, Alma Ata, means “full of apples.” By 1500 BC " +
+                    "apple seeds had been carried throughout Europe. The Greeks, Etruscans, and Romans cultivated " +
+                    "apples."},
+            {"banana", "Bananas were originally found in South East Asia, mainly in India. They were brought west " +
+                    "by Arab conquerors in 327 B.C. and moved from Asia Minor to Africa and finally carried to the " +
+                    "New World by the first explorers and missionaries to the Caribbean."},
+            {"strawberry", "The first garden strawberry was grown in Brittany, France, during the late 18th " +
+                    "century. Prior to this, wild strawberries and cultivated selections from wild strawberry " +
+                    "species were the common source of the fruit. The strawberry fruit was mentioned in ancient " +
+                    "Roman literature in reference to its medicinal use."},
+            {"orange", "The orange originated in a region encompassing Southern China, Northeast India, and Myanmar, " +
+                    "and the earliest mention of the sweet orange was in Chinese literature in 314 BC. As of 1987, " +
+                    "orange trees were found to be the most cultivated fruit tree in the world."},
+            {"pineapple", "Pineapple is believed to have originated in the Brazilian rainforests. Pineapples were " +
+                    "harvested by the native tribes and spread throughout South and Central America. When Christopher " +
+                    "Columbus landed in the new world in 1493, the Spaniards named the fruit 'piña' due to its " +
+                    "resemblance to a pinecone."},
+            {"kiwi", "The original fruit is from the Far East, having been grown in what is now modern-day China " +
+                    "for many centuries. It was only at the turn of the 20th Century, in 1904, that it arrived on " +
+                    "New Zealand shores, when New Zealand school principal Isabel Fraser brought some kiwifruit " +
+                    "seeds back from her travels."},
+            {"grape", "Archeological evidence suggests humans began growing grapes as early as 6500 B.C. during the " +
+                    "Neolithic era. By 4000 B.C., grape growing extended from Transcaucasia to Asia Minor and " +
+                    "through the Nile Delta of Egypt."},
+            {"blackberry", "The blackberry is an edible fruit produced by many species in the genus Rubus in the " +
+                    "family Rosaceae, hybrids among these species within the subgenus Rubus, and hybrids between " +
+                    "the subgenera Rubus and Idaeobatus. The taxonomy of blackberries has historically been " +
+                    "confused because of hybridization and apomixis, so that species have often been grouped " +
+                    "together and called species aggregates."},
+            {"mango", "Mangoes originated in India over 4,000 years ago and are considered a sacred fruit. " +
+                    "Mangoes spread gradually throughout Asia and then to the rest of the world. Due to a mangos " +
+                    "large center seed, the fruit relied on humans to transport them across the world."},
+            {"peach", "The peach probably originated in China and then spread westward through Asia to the " +
+                    "Mediterranean countries and later to other parts of Europe. The Spanish explorers took the " +
+                    "peach to the New World, and as early as 1600 the fruit was found in Mexico."}
     };
 
     private ArrayList<String> cart = new ArrayList<String>();
@@ -50,7 +84,7 @@ public class Main {
         System.out.println("----PRODUCTS----\n" +
                 "\tExit\n" + "\tReturn\n" + "\tView Cart");
         for(int i=0; i<products.length; i++){
-            System.out.println("\t- "+ products[0][i]);
+            System.out.println("\t- "+ products[i][0]);
         }
         System.out.print(">");
 
@@ -77,8 +111,8 @@ public class Main {
         System.out.println("----VIEW PRODUCT----\n" +
             "\tExit\n" + "\tReturn\n" + "\tAdd to cart");
         for(int i=0; i<products.length; i++){
-            if(products[0][i].equals(product)){
-                System.out.println(products[1][i]);
+            if(products[i][0].equals(product)){
+                System.out.println(products[i][1]);
             }
         }
         System.out.print(">");
